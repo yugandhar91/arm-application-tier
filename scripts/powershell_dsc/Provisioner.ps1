6a3ad3460e01acf7f250e4e63f2c548445e1d076
John Kerry <jk185160@ncr.com>
2017-11-17 10:17:40 -0500

    adding vault deployment

--- scripts/powershell_dsc/Provisioner.ps1
+++ scripts/powershell_dsc/Provisioner.ps1
@@ -2,12 +2,16 @@ Configuration Provisioner
 {
   param (
     [bool] $AttachDisk = $false
+    [bool] $WriteVault = $false
+    [string] $RoleID = "this_is_a_role_id"
+    [string] $SecretID = "this_is_a_secret_id"
   )
-  Import-DscResource -ModuleName xStorage
   
-  if($AttachDisk)
+  Import-DscResource -ModuleName xStorage
+
+  Node "localhost"
   {
-    Node "localhost"
+    if($AttachDisk)
     {
       xWaitforDisk Disk2
       {
@@ -15,11 +19,34 @@ Configuration Provisioner
         RetryIntervalSec = 20
         RetryCount = 30
       }
-      
+
       xDisk ADDataDisk {
         DiskId = 2
         DriveLetter = "F"
         DependsOn = "[xWaitForDisk]Disk2"
+      }
+    }
+    if($WriteVault)
+    {
+      $app_json = @{
+        "role_id"=$RoleID;
+        "secret_id"=$SecretID;
+      }
+
+      File hcvConfigFolder
+      {
+        Type = "Directory"
+        Ensure = "Present"
+        Recurse = $true
+        DestinationPath = "c:\chef\hash"
+      }
+
+      File appJson
+      {
+        Ensure = "Present"
+        Recurse = $true
+        DestinationPath = "c:\chef\hash\app.json"
+        Contents = (ConvertTo-Json $app_json)
       }
     }
   }
